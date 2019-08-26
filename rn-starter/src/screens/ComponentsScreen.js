44b5dd0bd53ca6b77aeffcaeea16ba57c3c024a5
Adnan Shahpurwala <ashahpur@eng.ucsd.edu>
2019-08-26 18:42:43 +0400

    updated

--- rn-starter/src/screens/ComponentsScreen.js
+++ rn-starter/src/screens/ComponentsScreen.js
@@ -1,13 +1,24 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
-const HomeScreen = () => {
-  return <Text style={styles.textStyle}>New Component</Text>;
+const ComponentsScreen = () => {
+  const name = 'Adnan';
+  return (
+  <View>
+    <Text style={styles.textStyle}>Getting started bitches</Text>
+    <Text style={styles.subHeaderStyle}>My name is {name} </Text>
+  </View>
+  
+
+  );
 };
 
 const styles = StyleSheet.create({
   textStyle: {
-    fontSize: 30
+    fontSize: 45
+  },
+  subHeaderStyle: {
+    fontSize: 20
   }
 });
 
---
+++ rn-starter/src/screens/ListScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const ListScreen = () => {
+  return (
+    <Text> List screen bitches </Text>
+  );
+};
+
+const styles = StyleSheet.create(
+  {
+
+  }
+);
+
+export default ListScreen;
